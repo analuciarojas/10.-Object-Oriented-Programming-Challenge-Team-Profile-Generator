e1334de0d992f39a2ed18cb7635fc3a74464fbe6
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-01-17 21:30:37 -0800

    finished adding styling for HTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,9 +8,9 @@ const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">local_cafe</i>
+            <div class="card-header" id="card-header">
+                <h3 id="h3">${manager.name}</h3>
+                <h4 id="h4">Manager</h4><i class="material-icons" id="i">local_cafe</i>
             </div>
             <div class="card-body">
                 <p>ID: ${manager.id}</p>
@@ -28,9 +28,9 @@ const generateEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">engineering</i>
+        <div class="card-header" id="card-header">
+            <h3 id="h3">${engineer.name}</h3>
+                <h4 id="h4">Engineer</h4><i class="material-icons" id="i">engineering</i>
             </div>
             <div class="card-body">
                 <p>ID: ${engineer.id}</p>
@@ -48,9 +48,9 @@ const generateIntern = function (intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
-            <div class="card-header">
-                <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">school</i>
+            <div class="card-header" id="card-header">
+                <h3 id="h3">${intern.name}</h3>
+                <h4 id="h4">Intern</h4><i class="material-icons" id="i">school</i>
             </div>
             <div class="card-body">
                 <p>ID: ${intern.id}</p>
@@ -120,12 +120,15 @@ const generateTeamPage = function (employeeCards) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;600&display=swap" rel="stylesheet">   
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <header>
-            <nav class="navbar">
-                <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="title">Team Profile</span>
             </nav>
         </header>
         <main>
