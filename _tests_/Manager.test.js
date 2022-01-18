91130ead377877fad0c14ea594f6c1c6d8f3e274
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-01-18 07:26:44 -0800

    added last manager test, and made sure all of them passed

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,18 @@
+// Manager constructor 
+
+const Manager = require('../lib/Manager');
+
+test("Can create Office Number", () => {
+    const testOfficeNumber = 123;
+    const employeeExample = new Manager("Ana Lucia", 1, "analurojasr@gmail.com", testOfficeNumber);
+    expect(employeeExample.officeNumber).toBe(testOfficeNumber);
+});
+
+// Testing role trough getRole 
+
+test("Gets role trough getRole", () => {
+    const returnValue = "Manager";
+    const employeeExample = new Manager("Ana Lucia", 1, "analurojasr@gmail.com", "analuciarojas");
+    expect(employeeExample.getRole()).toBe(returnValue);
+});
+
